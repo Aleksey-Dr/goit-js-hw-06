43d24925a7fb64b30e103c71ab9b548a136480a8
Aleksey-Dr <aprel181818@gmail.com>
2023-03-06 15:10:35 +0200

    task 9
    Task 1;
    Task 2;
    Task 3;
    Task 4;
    Task 5;
    Task 6;
    Task 7;
    Task 8;
    Task 9.

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const monitor = document.querySelector('.color');
+const changeColorBtn =
+  document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  monitor.textContent = body.style.backgroundColor;
+});
