fe7f530fc86305be7c8c3a5accc451d814888965
Aleksey-Dr <aprel181818@gmail.com>
2023-03-06 14:47:45 +0200

    task 8
    Tasc 1;
    Tasc 2;
    Tasc 3;
    Tasc 4;
    Tasc 5;
    Tasc 6;
    Tasc 7;
    Tasc 8.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+"use strict";
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    
+    email === '' || password === '' ?
+        alert('Please, fill in form fields') :
+        console.log(`Login: ${email}, Password: ${password}`);
+    
+    const formData = {
+        email,
+        password,
+    }
+    
+    event.currentTarget.reset();
+});
